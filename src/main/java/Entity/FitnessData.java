b460b79dcdfc9481391dead953f57bd5414fa894
artem <artemstupnickij8@gmail.com>
2025-05-27 14:16:20 +0300

    feat : create entity FitnessData

---
+++ src/main/java/Entity/FitnessData.java
@@ -0,0 +1,42 @@
+package Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "fitness_data") // замінив дефіс на нижнє підкреслення
+public class FitnessData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private int healthScore;  // або поясни, що це
+
+    private int pulse;
+
+    private int steps;
+
+    private double latitude;   // широта
+
+    private double longitude;  // довгота
+
+
+
+    public FitnessData (int id, int healthScore, int pulse, int steps, double latitude ) {
+        this.id = id;
+        this.healthScore = healthScore;
+        this.pulse = pulse;
+        this.steps = steps;
+        this.latitude = latitude;
+
+    }
+
+    public FitnessData () {
+
+    }
+
+}
+
